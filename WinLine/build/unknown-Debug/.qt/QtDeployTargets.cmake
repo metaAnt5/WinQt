fbff2ab036b0e8548b932f2a49a97357edab32a7
metaAnt5 <lxf_ant5@163.com>
2026-04-04 23:52:29 +0800

    11

--- WinLine/build/unknown-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_WinLine_FILE G:/qt/WinLine/build/unknown-Debug/WinLine.exe)
-set(__QT_DEPLOY_TARGET_WinLine_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_WinLine_RUNTIME_DLLS C:/Qt/6.7.2/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Cored.dll)
