d5803979c20993c7df3d8a285b0d77da5e0e4ce2
metaAnt5 <lxf_ant5@163.com>
2026-05-29 17:30:18 +0800

    cleanup: 删除模拟回放的标注持久化机制
    - 删除 loadAnnotations, saveTrades, saveShapes, saveAll 函数
    - 删除 closeEvent 中的保存调用
    - 删除 shapesChanged 信号连接
    - 删除 QFile/QJson 相关 include
    - 保留 KLineWidget 主图和画图工具栏功能不变

--- WinLine/simwindow.h
+++ WinLine/simwindow.h
@@ -22,9 +22,6 @@ public:
     explicit SimWindow(QWidget *parent = nullptr);
     ~SimWindow();
 
-protected:
-    void closeEvent(QCloseEvent *event) override;
-
 private slots:
     void onOpenFile();
     void onPlayPause();
@@ -41,14 +38,6 @@ private:
     void setupUi();
     void connectSignals();
     void resetSimulation();
-
-    // Annotation persistence
-    void loadAnnotations(const QString &csvFilePath);
-    void saveTrades();
-    void saveShapes();
-    void saveAll();
-    QString m_tradesFilePath;   // *.trades.json
-    QString m_shapesFilePath;   // *.shapes.json
 
     // UI 控件
     QComboBox *m_readerCombo;
